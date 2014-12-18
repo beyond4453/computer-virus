77f50b0b343a57d74b30047ea4cea57f2bfe613a
ZhipengG <beyond_acm@163.com>
2014-12-18 15:39:31 +0800

    modify the step3

--- lab2/step3/main.c
+++ lab2/step3/main.c
@@ -3,13 +3,28 @@
 #pragma comment(lib, "user32.lib")
 
 char *s = "I'm hacked";
-char *s1  = "user32.dll";
+//char *s1  = "user32.dll";
 char *s2  = "MessageBoxA";
 
 int main()
 {
 	__asm{
-		push s1 
+//		push s1 
+		sub esp, 0x20
+		mov  [ebp-0x20], 'u'
+		mov  [ebp-0x1f], 's'
+		mov  [ebp-0x1e], 'e'
+		mov  [ebp-0x1d], 'r'
+		mov  [ebp-0x1c], '3'
+		mov  [ebp-0x1b], '2'
+		mov  [ebp-0x1a], '.'
+		mov  [ebp-0x19], 'd'
+		mov  [ebp-0x18], 'l'
+		mov  [ebp-0x17], 'l'
+		mov  [ebp-0x16], 0x0
+		lea eax, [ebp-0x20]
+		push eax
+
 		mov  eax, dword ptr [LoadLibraryA]
 		call eax
 		push s2
@@ -21,6 +36,8 @@ int main()
 		push s
 		push 0
 		call eax
+
+		add esp, 0x20
 	}
 	return 0;
 
